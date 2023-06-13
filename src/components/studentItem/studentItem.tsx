a3d95a2d933d9b3eebe8bcfccaefd37f497f6ca4
Timz Mini <timz_mini@Timzs-Mac-mini.local>
2023-06-13 16:51:51 -0700

    finished grid view for student item

--- src/components/studentItem/studentItem.tsx
+++ src/components/studentItem/studentItem.tsx
@@ -17,83 +17,86 @@ const StudentItem = (props: StudentItemProps) => {
 
   return (
     <div key={props.student.id} className={classes.student}>
-      {/* STUDENT VIEW BUTTON */}
-      <button
-        className={`btn ${
-          props.student.active ? classes.activeButton : classes.inactiveButton
-        } ${classes.iconButton}`}
-        data-bs-toggle="modal"
-        data-bs-target="#viewStudentModal"
-        onClick={() => {
-          props.udpateCurrentStudent();
-        }}
-      >
-        <i className="bi bi-person-fill"></i>
-      </button>
-      {/* STUDENT NAME */}
-      <span className={`${classes.studentText} ${classes.studentName}`}>
-        {props.student.first_name} {props.student.last_name}
-      </span>
-      {/* STUDENT PHONE 1 */}
-      {props.student.phone_1 ? (
-        // <span className={`${classes.phoneButton} ${classes.studentPhone}`}>
-        <a
-          className={`btn ${classes.phoneButton} ${classes.phone1}`}
-          href={`tel:${props.student.phone_1}`}
-          target="_blank"
-        >
-          {formatPhoneNumber(props.student.phone_1)}
-        </a>
-      ) : // </span>
-      null}
-      {/* STUDENT PHONE 2*/}
-      {props.student.phone_2 ? (
-        // <span className={`${classes.phoneButton} ${classes.studentPhone}`}>
-        <a
-          className={`btn ${classes.phoneButton} ${classes.phone2}`}
-          href={`tel:${props.student.phone_2}`}
-          target="_blank"
-        >
-          {formatPhoneNumber(props.student.phone_2)}
-        </a>
-      ) : // </span>
-      null}
-
-      {/* STUDENT EDITOR */}
-      <div
-        className={`${classes.editor} ${
-          showEditor ? classes.editorIn : classes.editorOut
-        }`}
-      >
+      {/* GRID STUDENT INFO */}
+      <div className={classes.studentInfo}>
+        {/* STUDENT VIEW BUTTON */}
         <button
-          className={`btn  ${classes.studentButton} ${classes.editButton}`}
+          className={`btn ${
+            props.student.active ? classes.activeButton : classes.inactiveButton
+          } ${classes.iconButton}`}
           data-bs-toggle="modal"
-          data-bs-target="#updateStudentModal"
+          data-bs-target="#viewStudentModal"
           onClick={() => {
-            props.editHandler();
+            props.udpateCurrentStudent();
           }}
         >
-          <span>Edit</span>
-          <i className="bi bi-pencil-fill"></i>
+          <i className="bi bi-person-fill"></i>
         </button>
-        <button
-          className={`btn  ${classes.studentButton}  ${classes.deleteButton}`}
-          onClick={() => {
-            props.deleteHandler();
-          }}
-        >
-          <span>Delete</span>
-          <i className="bi bi-trash-fill"></i>
-        </button>
+        {/* STUDENT NAME */}
+        <span className={`${classes.studentName}`}>
+          {props.student.first_name} {props.student.last_name}
+        </span>
       </div>
-      {/* STUDENT EDITOR BUTTON*/}
+      {/* GRID PHONE NUMBERS */}
+      <div className={classes.phoneNumbers}>
+        {/* STUDENT PHONE 1 */}
+        {props.student.phone_1 ? (
+          <a
+            className={`btn ${classes.phoneButton}`}
+            href={`tel:${props.student.phone_1}`}
+            target="_blank"
+          >
+            {formatPhoneNumber(props.student.phone_1)}
+          </a>
+        ) : null}
+        {/* STUDENT PHONE 2*/}
+        {props.student.phone_2 ? (
+          <a
+            className={`btn ${classes.phoneButton}`}
+            href={`tel:${props.student.phone_2}`}
+            target="_blank"
+          >
+            {formatPhoneNumber(props.student.phone_2)}
+          </a>
+        ) : null}
+      </div>
+      {/* GRID EDITOR */}
+      <div className={classes.studentEditor}>
+        <div
+          className={`${classes.editor} ${
+            showEditor ? classes.editorIn : classes.editorOut
+          }`}
+        >
+          <button
+            className={`btn ${classes.studentButton} ${classes.editButton}`}
+            data-bs-toggle="modal"
+            data-bs-target="#updateStudentModal"
+            onClick={() => {
+              props.editHandler();
+            }}
+          >
+            <span>Edit</span>
+            <i className="bi bi-pencil-fill"></i>
+          </button>
+          <button
+            className={`btn  ${classes.studentButton}  ${classes.deleteButton}`}
+            onClick={() => {
+              props.deleteHandler();
+            }}
+          >
+            <span>Delete</span>
+            <i className="bi bi-trash-fill"></i>
+          </button>
+        </div>
+      </div>
+      {/* GRID EDITOR BUTTON*/}
       <div
-        className={classes.showEditor}
+        className={classes.showEditorButton}
         onClick={() => {
           setShowEditor(!showEditor);
         }}
       >
-        ...
+        <i className="bi bi-pencil-fill"></i>
       </div>
     </div>
   );
